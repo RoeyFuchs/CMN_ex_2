75b2503fa587bb42df82016cfc8d437edca35c97
fuchsroey <fuchsroey@gmail.com>
2020-05-22 11:40:53 +0300

    kfold done

--- kfold.m
+++ kfold.m
@@ -1,6 +1,4 @@
-function kFolded = SVMtrial(x,y,k)
-  % data = [x y];
-
+function kFolded = kfold(x,y,k)
   total_sample = size(y);
   total_sample = total_sample(1);
 
@@ -27,19 +25,26 @@ function kFolded = SVMtrial(x,y,k)
       currentK = currentK+1;
     end
   end
-  kFolded.X = zeros([k*num_of_sampels_in_set-num_of_sampels_in_set xtype k]);
-  kFolded.Y = zeros([num_of_sampels_in_set k]);
+
+  Data.Y = Data.Y';
+
+  kFolded.train.X = zeros([(k-1)*num_of_sampels_in_set xtype k]); % n/k * (k-1), 2, k
+  kFolded.train.Y = zeros([(k-1)*num_of_sampels_in_set k])';
+  kFolded.test.X = zeros([num_of_sampels_in_set xtype k]);
+  kFolded.test.Y = zeros([num_of_sampels_in_set k])';
+
 
   index_of_k = 1:k;
   for i = 1:k
-    kFolded.X(:,:,i) = Data.X(:,:,i)
-
-
-
+    kFolded.test.X(:,:,i) = Data.X(:,:,i);
+    kFolded.test.Y(i,:) = Data.Y(i,:);
+    m = 0;
+    for j = index_of_k(index_of_k~=i)
+      kFolded.train.X((m*num_of_sampels_in_set)+1:((m+1)*num_of_sampels_in_set),:,i) = Data.X(:,:,j);
+      kFolded.train.Y(i,(m*num_of_sampels_in_set)+1:((m+1)*num_of_sampels_in_set)) = Data.Y(j,:);
+      m = m+1;
+    end
   end
-
-
-
 
 
 end
